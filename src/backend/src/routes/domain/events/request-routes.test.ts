e7377a0194ad8b1430dd80fe82ff46b43a6cdac7
granchetti <118294811+granchetti@users.noreply.github.com>
2025-07-01 09:42:11 +0200

    Add Address value object and update RequestRoutes

--- src/backend/src/routes/domain/events/request-routes.test.ts
+++ src/backend/src/routes/domain/events/request-routes.test.ts
@@ -1,16 +1,17 @@
 import { RequestRoutesEvent } from './request-routes';
 import { RouteId } from '../value-objects/route-id-value-object';
+import { Address } from '../value-objects/address-value-object';
 
 describe('RequestRoutesEvent', () => {
   it('should store provided properties', () => {
     const routeId = RouteId.generate();
     const event = new RequestRoutesEvent({
       routeId,
-      origin: 'A',
-      destination: 'B',
+      origin: new Address('A'),
+      destination: new Address('B'),
     });
     expect(event.routeId).toBe(routeId);
-    expect(event.origin).toBe('A');
-    expect(event.destination).toBe('B');
+    expect(event.origin.Value).toBe('A');
+    expect(event.destination.Value).toBe('B');
   });
 });
--- src/backend/src/routes/domain/events/request-routes.ts
+++ src/backend/src/routes/domain/events/request-routes.ts
@@ -1,15 +1,16 @@
 import { RouteId } from '../value-objects/route-id-value-object';
+import { Address } from '../value-objects/address-value-object';
 
 export interface RequestRoutesProps {
   readonly routeId: RouteId;
-  readonly origin: string;
-  readonly destination: string;
+  readonly origin: Address;
+  readonly destination: Address;
 }
 
 export class RequestRoutesEvent {
   readonly routeId: RouteId;
-  readonly origin: string;
-  readonly destination: string;
+  readonly origin: Address;
+  readonly destination: Address;
 
   constructor(props: RequestRoutesProps) {
     this.routeId = props.routeId;
