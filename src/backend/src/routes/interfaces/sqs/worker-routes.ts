23b62d7645a146b6854b6bb7704ae3f9e25bd2fb
granchetti1 <134518621+granchetti1@users.noreply.github.com>
2025-07-08 16:54:08 +0200

    fix: worker routes

--- src/backend/src/routes/interfaces/sqs/worker-routes.ts
+++ src/backend/src/routes/interfaces/sqs/worker-routes.ts
@@ -143,8 +143,7 @@ async function computeRoutes(
       },
     },
     travelMode: "WALK",
-    computeAlternativeRoutes: true,
-    routingPreference: "UNRESTRICTED",
+    computeAlternativeRoutes: true
   };
 
   console.info("🌐 Calling Routes API with coords…", requestBody);
