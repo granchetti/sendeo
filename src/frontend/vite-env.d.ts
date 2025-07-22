56a99ba7d3bdb925ac34e4c5f892d20b5129f5e8
granchetti <118294811+granchetti@users.noreply.github.com>
2025-07-22 13:49:34 +0200

    feat: poll for generated routes

--- src/frontend/vite-env.d.ts
+++ src/frontend/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_USER_POOL_ID: string
   readonly VITE_CLIENT_ID: string
   readonly VITE_REGION: string
+  readonly VITE_GOOGLE_MAPS_API_KEY: string
 }
 
 interface ImportMeta {
