16b20107dcc5ce042f58d2abe23fd8c6ede825d8
granchetti <118294811+granchetti@users.noreply.github.com>
2025-09-02 15:28:39 +0200

    Add domain events and dispatcher

--- src/backend/src/routes/domain/events/route-generated.ts
+++ src/backend/src/routes/domain/events/route-generated.ts
@@ -1,13 +1,16 @@
-import { Route } from '../entities/route-entity';
+import { DomainEvent } from "../../../shared/domain/events/domain-event";
+import { Route } from "../entities/route-entity";
 
 export interface RouteGeneratedProps {
   readonly route: Route;
 }
 
-export class RouteGeneratedEvent {
+export class RouteGeneratedEvent extends DomainEvent {
+  readonly eventName = "RouteGenerated";
   readonly route: Route;
 
   constructor(props: RouteGeneratedProps) {
+    super();
     this.route = props.route;
   }
 }
