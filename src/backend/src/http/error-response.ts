06c15abd3d329da844dff216ca125cc544a71216
granchetti1 <134518621+granchetti1@users.noreply.github.com>
2025-09-07 11:22:59 +0200

    (BE) update error-repsonse.ts

--- src/backend/src/http/error-response.ts
+++ src/backend/src/http/error-response.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyResult } from "aws-lambda";
-import { corsHeaders } from "./cors";
+import { jsonHeaders } from "./cors";
 
 export function errorResponse(
   code: number,
@@ -12,7 +12,7 @@ export function errorResponse(
   }
   return {
     statusCode: code,
-    headers: corsHeaders,
+    headers: jsonHeaders,
     body: JSON.stringify(body),
   };
 }
