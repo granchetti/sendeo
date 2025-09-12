625511df090c2c33c339ff64eadcf3f076eb531e
granchetti <118294811+granchetti@users.noreply.github.com>
2025-09-12 21:53:13 +0200

    feat: allow request routes to write to routes table

--- infrastructure/lib/stacks/compute-stack.ts
+++ infrastructure/lib/stacks/compute-stack.ts
@@ -90,6 +90,13 @@ export class ComputeStack extends cdk.Stack {
     });
     props.routeJobsQueue.grantSendMessages(requestRoutes.fn);
 
+    requestRoutes.fn.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["dynamodb:PutItem"],
+        resources: [props.routesTable.tableArn],
+      })
+    );
+
     // 2) WorkerRoutes → routeJobsQueue consumer
     const workerRoutes = new SqsConsumer(this, "WorkerRoutes", {
       entry: path.join(
